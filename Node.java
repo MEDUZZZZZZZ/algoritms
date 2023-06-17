31673fd7a4040a367d965bdd912aab9554194c42
Oleg <popovolegvrn@gmail.com>
2023-06-17 15:40:49 +0300

    Added Node.java

---
+++ Node.java
@@ -0,0 +1,12 @@
+package algoritms;
+
+public class Node <K, V>{
+    K key;
+    V value;
+    Node<K, V> nextnode;
+
+    public Node(K key, V value) {
+        this.key = key;
+        this.value = value;
+    }
+}
